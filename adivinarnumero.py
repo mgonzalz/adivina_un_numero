294a36464c04e0ba19c113ce04390cf13eb4900f
mgonzalz <mgonzgarc@myuax.com>
2022-11-04 18:27:51 +0100

    codigo

--- adivinarnumero.py
+++ adivinarnumero.py
@@ -1,13 +1,9 @@
 #Elegir número
-numero = input("Adivine el numero entre el 0 y el 99: ")
+numero = int(input("Adivine el numero entre el 0 y el 99: "))
 #Elección numero al azar
-import random 
+import random
 numeroal = random.randint(0, 100)
-#Comprobacion de numero
-try numero = int(numero)
-while not 0<=numero<=99:
-    numero = input("Adivine el numero entre el 0 y el 99: ")
-    numero = int(numero)
+numeroal = int(numeroal)
 #Intento
 while True:
     if numero == numeroal:
